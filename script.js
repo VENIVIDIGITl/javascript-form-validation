bfacb7838b7c8bb6801610e48f2b896334b42fb0
VeniVidiGiti <99083579+VENIVIDIGITl@users.noreply.github.com>
2022-04-27 23:17:06 +0300

    add validations to check if passwords match and form is valid, respond with a success or an error message

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
 let isValid = false;
+let passwordsMatch = false;
 
 
 function validateForm() {
@@ -15,6 +16,27 @@ function validateForm() {
     message.textContent = 'Please fill out all fields.';
     message.style.color = '#dc143c';
     messageContainer.style.borderColor = '#dc143c';
+    return;
+  }
+  // Check to see if passwords match
+  if (password1El.value === password2El.value) {
+    passwordsMatch = true;
+    password1El.style.borderColor = '#2da778';
+    password2El.style.borderColor = '#2da778';
+  } else {
+    passwordsMatch = false;
+    message.textContent = 'Make sure passwords match.';
+    message.style.color = '#dc143c';
+    messageContainer.style.borderColor = '#dc143c';
+    password1El.style.borderColor = '#dc143c';
+    password2El.style.borderColor = '#dc143c';
+    return;
+  }
+  // If form is valid and passwords match
+  if (isValid && passwordsMatch) {
+    message.textContent = 'Successfully Registered!';
+    message.style.color = '#2da778';
+    messageContainer.style.borderColor = '#2da778';
   }
 }
 
