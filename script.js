9cfddc7d42193da0730bd716f17f573a259cf571
VeniVidiGiti <99083579+VENIVIDIGITl@users.noreply.github.com>
2022-04-27 22:59:29 +0300

    change color for invalid field borders and error message, addEventListener() to the form to check validation

---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+
+
+function validateForm() {
+  // Using Constraint API
+  isValid = form.checkValidity();
+  // Style main message for an error
+  if (!isValid) {
+    message.textContent = 'Please fill out all fields.';
+    message.style.color = '#dc143c';
+    messageContainer.style.borderColor = '#dc143c';
+  }
+}
+
+
+function processFormData(event) {
+  event.preventDefault();
+  // Validate Form
+  validateForm();  
+}
+
+
+// Event Listener
+form.addEventListener('submit', processFormData);
