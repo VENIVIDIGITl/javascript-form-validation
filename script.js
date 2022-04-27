33339b84fbceb955ad0fb3f2c3a02d54ab29aa39
VeniVidiGiti <99083579+VENIVIDIGITl@users.noreply.github.com>
2022-04-28 00:09:05 +0300

    code cleanup

--- script.js
+++ script.js
@@ -8,14 +8,19 @@ let isValid = false;
 let passwordsMatch = false;
 
 
+function displayMessage(messageText, color) {
+  message.textContent = messageText;
+  message.style.color = color;
+  messageContainer.style.borderColor = color;
+}
+
+
 function validateForm() {
   // Using Constraint API
   isValid = form.checkValidity();
   // Style main message for an error
   if (!isValid) {
-    message.textContent = 'Please fill out all fields.';
-    message.style.color = '#dc143c';
-    messageContainer.style.borderColor = '#dc143c';
+    displayMessage('Please fill out all fields.', '#dc143c');
     return;
   }
   // Check to see if passwords match
@@ -25,18 +30,14 @@ function validateForm() {
     password2El.style.borderColor = '#2da778';
   } else {
     passwordsMatch = false;
-    message.textContent = 'Make sure passwords match.';
-    message.style.color = '#dc143c';
-    messageContainer.style.borderColor = '#dc143c';
+    displayMessage('Make sure passwords match.', '#dc143c')
     password1El.style.borderColor = '#dc143c';
     password2El.style.borderColor = '#dc143c';
     return;
   }
   // If form is valid and passwords match
   if (isValid && passwordsMatch) {
-    message.textContent = 'Successfully Registered!';
-    message.style.color = '#2da778';
-    messageContainer.style.borderColor = '#2da778';
+    displayMessage('Successfully Registered!', '#2da778');
   }
 }
 
